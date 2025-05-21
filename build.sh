984b494ebf2469afd669d135009d6625b25e63d6
Au <101819136+oaurumo@users.noreply.github.com>
2025-05-22 02:29:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ VERSION_TYPE="${VERSION_TYPE:-standard}"
 if [ "$VERSION_TYPE" = "debian12_minimal" ]; then
   echo "构建debian12_minimal-armbian..."
   FILE_NAME="Armbian_25.2.1_Uefi-x86_bookworm_current_6.12.13_minimal.img.xz"
-elif [ "$VERSION_TYPE" = "ubuntu24_minimal" ]; then
-  echo "构建ubuntu24_minimal-armbian..." 
-  FILE_NAME="Armbian_25.2.1_Uefi-x86_noble_current_6.12.13_minimal.img.xz"
+elif [ "$VERSION_TYPE" = "ubuntu22_minimal" ]; then
+  echo "构建ubuntu22_minimal-armbian..." 
+  FILE_NAME="Armbian_25.2.1_Uefi-x86_jammy_current_6.12.13_minimal.img.xz"
 elif [ "$VERSION_TYPE" = "homeassistant_debian12_minimal" ]; then
   echo "构建homeassistant全家桶版armbian..." 
   FILE_NAME="Armbian_25.2.3_Uefi-x86_bookworm_current_6.12.17-homeassistant_minimal.img.xz"
